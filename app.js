70e40003fa7136dbff593f2702704f30651d7336
AlaaMarei <>
2021-06-17 08:26:27 +0000

    2nd update

---
+++ app.js
@@ -0,0 +1,18 @@
+alert("Welcome to the Movies land Totally CUSTOMIZED for you");
+var name = prompt ("Whats your name ?")
+var  birth month =  prompt ("THE MONTH NUMBER YOU BORN IN ..like june,july ..etc ?")
+var  birth day =  prompt ("THE DAY NUMBER YOU BORN IN .. like 1,2,3 .. etc ?")
+
+if(birth month == "janury") {document.write ("<p>)"+"your zodic sign is |EARTH| so we suggest the DRAMA genre for YOU"+"</P>"}
+else IF (birth month == "February") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the FANTASY genre for YOU"+"</P>"}
+else IF (birth month == "March") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the Romance genre for YOU"+"</P>"}
+else IF (birth month == "April") {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Horror genre for YOU"+"</P>"}
+else IF (birth month == "May") {document.write ("<p>)"+"your zodic sign is |Earth| so we suggest the Comedy genre for YOU"+"</P>"}
+else IF (birth month == "June") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the Romance genre for YOU"+"</P>"}
+else IF (birth month == "July") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the FANTASY genre for YOU"+"</P>"}
+else IF (birth month == "August") {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Action genre for YOU"+"</P>"}
+else IF (birth month == "September") {document.write ("<p>)"+"your zodic sign is |Earth| so we suggest the Drama genre for YOU"+"</P>"}
+else IF (birth month == "October") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the FANTASY genre for YOU"+"</P>"}
+else IF (birth month == "November") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the Fantasy genre for YOU"+"</P>"}
+else  (birth month == "December") {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Horror genre for YOU"+"</P>"}
+
