0dbe9a38b0bebf3115e44bea5200bda578a2de5d
Alaa90-90 <85881471+Alaa90-90@users.noreply.github.com>
2021-06-19 05:19:38 +0300

    update

--- app.js
+++ app.js
@@ -4,31 +4,44 @@ alert("wlecome")
 
 var name = "khaleel"
 
+var birthMonth = "fffff"
 
 var name = prompt ("Whats your name ?")
 
 
-alert("welcome" + name )
+alert("welcome " + name )
 
 
-var birthMonth =  prompt ("THE MONTH NUMBER YOU BORN IN ..like june,july ..etc ?")
+var birthMonth = prompt("THE MONTH YOU BORN IN (jtanuary,february,march,april,may,june,july,august,september,october,november,december)?")
 
-var birthDay =  prompt ("THE DAY NUMBER YOU BORN IN .. like 1,2,3 .. etc ?")
+var genre = prompt("whats your favorite Movie genre ?")
 
- alert("Welcome to the Movies land Totally CUSTOMIZED for you");
+while(genre != "action" && genre != "comedy" && genre != "drama" && genre != "fantasy" && genre != "horror" && genre != "romance" ){
+  genre =prompt("WE DO NOT HAVE THIS GENRE... PLEASE TRY ANOTHER ONE")
+}
+alert("ACCORDING TO YOUR ZODIAC SIGN WE SUGGEST FOR YOU .......")
+// if(birthMonth == "february" || birthMonth == "june" || birthMonth == "october"){
+//   var holyday = prompt("how many day off you have in your weekend to watch your favorite movies ?")
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"THE SECRET OF MOONACRE"+
+//     "</h2>"+"<img src'https://i.pinimg.com/originals/23/24/7e/23247e5492975667ada71dc12721bb41.jpg'>"+"</div>")
+//   }
+// }
+// else if (birthMonth == "april" || birthMonth == "august" || birthMonth == "december"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"THE CONJURING"+
+//     "</h2>"+"<img src'https://resizing.flixster.com/g96ZK8bkJAkQIAdSJ5QKm6fIoNU=/206x305/v2/https://flxt.tmsimg.com/NowShowing/120409/120409_aa.jpg'>"+"</div>")
+//     }
+//     else if (birthMonth == "january" || birthMonth == "may" || birthMonth == "september"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"THE SHAWSHANK REDEMPTION"+
+//     "</h2>"+"<img src'https://goodmovieslist.com/best-movies/movie-posters/tt0111161.jpg'>"+"</div>")
+//     }
+//     else  (birthMonth == "march" || birthMonth == "july" || birthMonth == "november"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"ARTIST"+
+//     "</h2>"+"<img src'https://resizing.flixster.com/2TTCt4MuxsxZYUK26WeCN1FPIM4=/206x305/v2/https://flxt.tmsimg.com/assets/p8725913_p_v13_aj.jpg'>"+"</div>")
+//     }
+//     }
 
-
-if(birthMonth == "janury") {document.write("<p>)"+"your zodic sign is |EARTH| so we suggest the DRAMA genre for YOU"+"</P>")}else if (birthmonth == "February") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the FANTASY genre for YOU"+"</P>")}
-else if (birthmonth == "March") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the Romance genre for YOU"+"</P>")}
-else if (birthmonth == "April") {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Horror genre for YOU"+"</P>")}
-else if (birthmonth == "May") {document.write ("<p>)"+"your zodic sign is |Earth| so we suggest the Comedy genre for YOU"+"</P>")}
-else if (birthmonth == "June") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the Romance genre for YOU"+"</P>")}
-else if (birthmonth == "July") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the FANTASY genre for YOU"+"</P>")
-}else if (birthmonth == "August") {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Action genre for YOU"+"</P>")}
-else if (birthmonth == "September") {document.write ("<p>)"+"your zodic sign is |Earth| so we suggest the Drama genre for YOU"+"</P>")
-}else if (birthmonth == "October") {document.write ("<p>)"+"your zodic sign is |AIR| so we suggest the FANTASY genre for YOU"+"</P>")}
-else if (birthmonth == "November") {document.write ("<p>)"+"your zodic sign is |Water| so we suggest the Fantasy genre for YOU"+"</P>")}
-else  {document.write ("<p>)"+"your zodic sign is |Fire| so we suggest the Horror genre for YOU"+"</P>")}
-
-
-
+// alert("Welcome to the Movies land Totally CUSTOMIZED for you")
