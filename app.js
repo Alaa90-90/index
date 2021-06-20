32a6a8f02376ed79da44b4ad76c820561935ec0a
AlaaMarei <>
2021-06-20 11:41:32 +0000

    adding

--- app.js
+++ app.js
@@ -20,32 +20,49 @@ while(genre != "action" && genre != "comedy" && genre != "drama" && genre != "fa
   genre =prompt("WE DO NOT HAVE THIS GENRE... PLEASE TRY ANOTHER ONE")
 }
 alert("ACCORDING TO YOUR ZODIAC SIGN WE SUGGEST FOR YOU .......")
-if(birthMonth == "february" || birthMonth == "june" || birthMonth == "october"){
+
+
+function freetime (){
+
   var holyday = prompt("how many day off you have in your weekend to watch your favorite movies ?")
+
+  holyday = parseInt(holyday)
   for (var l = 1; l <= holyday; l++){
     document.write("<div>"+"<h2>"+"THE SECRET OF MOONACRE"+
-    "</h2>"+"<img src'https://i.pinimg.com/originals/23/24/7e/23247e5492975667ada71dc12721bb41.jpg'>"+"</div>")
+    "</h2>"+"<img src='https://i.pinimg.com/originals/23/24/7e/23247e5492975667ada71dc12721bb41.jpg'>"+"</div>")
   }
+
 }
-else if (birthMonth == "april" || birthMonth == "august" || birthMonth == "december"){
-  for (var l = 1; l <= holyday; l++){
-    document.write("<div>"+"<h2>"+"THE CONJURING"+
-    "</h2>"+"<img src'https://resizing.flixster.com/g96ZK8bkJAkQIAdSJ5QKm6fIoNU=/206x305/v2/https://flxt.tmsimg.com/NowShowing/120409/120409_aa.jpg'>"+"</div>")
-    }
-    else if (birthMonth == "january" || birthMonth == "may" || birthMonth == "september"){
-  for (var l = 1; l <= holyday; l++){
-    document.write("<div>"+"<h2>"+"THE SHAWSHANK REDEMPTION"+
-    "</h2>"+"<img src'https://goodmovieslist.com/best-movies/movie-posters/tt0111161.jpg'>"+"</div>")
-    }
-    else  (birthMonth == "march" || birthMonth == "july" || birthMonth == "november"){
-  for (var l = 1; l <= holyday; l++){
-    document.write("<div>"+"<h2>"+"ARTIST"+
-    "</h2>"+"<img src'https://resizing.flixster.com/2TTCt4MuxsxZYUK26WeCN1FPIM4=/206x305/v2/https://flxt.tmsimg.com/assets/p8725913_p_v13_aj.jpg'>"+"</div>")
-    }
-    }
+
+freetime();
+
+//dfgdfgsdg
+
+
+// if(birthMonth == "february" || birthMonth == "june" || birthMonth == "october"){
+
+// }
+// else if (birthMonth == "april" || birthMonth == "august" || birthMonth == "december"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"THE CONJURING"+
+//     "</h2>"+"<img src'https://resizing.flixster.com/g96ZK8bkJAkQIAdSJ5QKm6fIoNU=/206x305/v2/https://flxt.tmsimg.com/NowShowing/120409/120409_aa.jpg'>"+"</div>")
+//     }
+//     else if (birthMonth == "january" || birthMonth == "may" || birthMonth == "september"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"THE SHAWSHANK REDEMPTION"+
+//     "</h2>"+"<img src'https://goodmovieslist.com/best-movies/movie-posters/tt0111161.jpg'>"+"</div>")
+//     }
+//     else  (birthMonth == "march" || birthMonth == "july" || birthMonth == "november"){
+//   for (var l = 1; l <= holyday; l++){
+//     document.write("<div>"+"<h2>"+"ARTIST"+
+//     "</h2>"+"<img src'https://resizing.flixster.com/2TTCt4MuxsxZYUK26WeCN1FPIM4=/206x305/v2/https://flxt.tmsimg.com/assets/p8725913_p_v13_aj.jpg'>"+"</div>")
+//     }
+//     }
 
       
       
       
 alert("Welcome to the Movies land Totally CUSTOMIZED for you")
+
+
 
